9c8dd40b747ae1a4bb258fe29df9222a0e82d649
Leandra Großpietsch <101011803+leandra-gg@users.noreply.github.com>
2025-06-27 19:27:23 +0200

    final changes, removed test classes

--- scripts/checkBalance.js
+++ scripts/checkBalance.js
@@ -4,14 +4,14 @@ async function main() {
   const [signer] = await ethers.getSigners();
   console.log("Wallet:", signer.address);
 
-  // ETH-Balance abrufen (nativer Token auf OP Sepolia)
+  // get balance
   const balance = await signer.provider.getBalance(signer.address);
 
   console.log(`ETH Balance on OP Sepolia: ${ethers.formatEther(balance)} ETH`);
 }
 
 main().catch((err) => {
-  console.error("Fehler beim Abrufen der ETH-Balance:", err);
+  console.error("Error:", err);
   process.exit(1);
 });
 
